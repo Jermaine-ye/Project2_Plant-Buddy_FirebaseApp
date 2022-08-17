d1b90955a3d567978a1b06159b9f98bf783bbf03
starstormfall <photon_dazzles_02@icloud.com>
2022-08-17 17:55:25 +0800

    set up api call to database for user's plants

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,33 @@
-import { auth } from "../DB/firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { UserContext } from "../App";
+
+// imports for react
 import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { UserContext } from "../App";
+
+// imports for firebase
+import { auth, database } from "../DB/firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { ref as databaseRef, onChildAdded } from "firebase/database";
+
+// folders in realtime database
+const USER_PLANT_FOLDER_NAME = "userPlants/";
 
 export default function Dashboard(props) {
   // const [user, setUser] = useState(null);
   const user = useContext(UserContext);
   let plantsInfo = [1, 2, 3, 4, 5, 6]; // REPLACE THIS WITH DATABASE PLANT PROFILES
 
-  const navigate = useNavigate();
-  const logout = () => {
-    signOut(auth).then(() => {
-      navigate("/login");
-    });
-  };
+  const [userPlants, setUserPlants] = useState([]);
 
+  const navigate = useNavigate();
+
+  // for testing purpose
+  const userEmail = "abc@abc.com";
+  const userName = "def";
+
+  // includes checking of auth status and load user's plants from realtime database
   useEffect(() => {
+    // for checking of user logged in status
     onAuthStateChanged(auth, (signedInUser) => {
       if (signedInUser) {
         const uid = signedInUser.uid;
@@ -25,10 +36,41 @@ export default function Dashboard(props) {
         navigate("/login");
       }
     });
-  });
+
+    const userPlantRef = databaseRef(
+      database,
+      USER_PLANT_FOLDER_NAME + "/" + userName
+    );
+
+    onChildAdded(userPlantRef, (data) => {
+      const species = Object.keys(data.val())[0];
+      const speciesInfo = data.val()[species];
+      setUserPlants((prevPostsState) => [
+        ...prevPostsState,
+        { key: species, val: speciesInfo },
+      ]);
+    });
+
+    return () => {
+      setUserPlants([]);
+    };
+  }, []);
+
+  const logout = () => {
+    signOut(auth).then(() => {
+      navigate("/login");
+    });
+  };
+
+  // to render user's list of plants
+
+  const plantCard = userPlants.map((plant, index) => (
+    <div key={index}> {plant.key} </div>
+  ));
 
   return (
     <div>
+      {/* DO NOT TOUCH */}
       <div>
         {user ? <h2>Good morning, {user.email}</h2> : null}
         <button
@@ -46,8 +88,11 @@ export default function Dashboard(props) {
       <div>
         <h3>Weather API placeholder</h3>
       </div>
+
+      {/* TO EDIT: list user's plants */}
       <div>
         Plant Profiles
+        {plantCard}
         <ul>
           {plantsInfo.map((id) => {
             return (
@@ -58,6 +103,8 @@ export default function Dashboard(props) {
           })}
         </ul>
       </div>
+
+      {/* DO NOT TOUCH */}
       <Link to={`/addnewplant`}>
         <button>Add A New Plant!</button>
       </Link>
--- src/components/PlantInfo.js
+++ src/components/PlantInfo.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
+// imports related to realtime database
+import { ref, child, get } from "firebase/database";
+import { database } from "../DB/firebase";
+
 export default function PlantInfo(props) {
   // const [state, setState] = useState(state)
   const { id } = useParams();
